664c2aaa98abe0f9470870bce4f4940e9028724a
Diogo Mendes <up202005393@up.pt>
2024-01-04 21:43:36 +0000

    Report Activity almost finished

--- SnorLacksApp/app/src/main/java/com/snorlacks/snorlacksapp/SleepReportActivity.java
+++ SnorLacksApp/app/src/main/java/com/snorlacks/snorlacksapp/SleepReportActivity.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.navigation.NavigationBarView;
 import com.jjoe64.graphview.DefaultLabelFormatter;
@@ -32,11 +33,19 @@ import java.util.Locale;
 public class SleepReportActivity extends AppCompatActivity {
 
     GraphView graphView;
+
+    Toolbar toolbar;
     TextView textViewApneaEvents;
     TextView textViewSleepStamps;
     TextView textViewSleepQuality;
     ImageView imageViewSleepQuality;
     ConstraintLayout constraintLayout;
+    TextView textViewFallAsleepTime;
+    TextView textViewSleepTime;
+    TextView textViewAwakeningTime;
+    TextView textViewMinBPM;
+    TextView textViewMedianBPM;
+    TextView textViewMaxBPM;
 
     SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, MMM d, ''yy");
     SimpleDateFormat timeStampFormat = new SimpleDateFormat("h:mm a");
@@ -65,12 +74,20 @@ public class SleepReportActivity extends AppCompatActivity {
         dbHandler = DBHandler.getInstance(SleepReportActivity.this);
 
         constraintLayout = findViewById(R.id.constraintLayout);
+        toolbar = findViewById(R.id.toolbar);
         textViewApneaEvents = findViewById(R.id.txtApneaEvents);
         graphView = findViewById(R.id.idGraphView);
         textViewApneaEvents = findViewById(R.id.txtApneaEvents);
         textViewSleepStamps = findViewById((R.id.txtSleepStamps));
         textViewSleepQuality = findViewById((R.id.txtSleepQuality));
         imageViewSleepQuality = findViewById((R.id.imgSleepQuality));
+
+        textViewFallAsleepTime = findViewById(R.id.txtViewFallTime);
+        textViewSleepTime = findViewById(R.id.txtViewSleepTime);
+        textViewAwakeningTime = findViewById(R.id.txtViewAwakeningTime);
+        textViewMinBPM = findViewById(R.id.txtViewMinHRValue);
+        textViewMedianBPM = findViewById(R.id.txtViewMedianHRValue);
+        textViewMaxBPM = findViewById(R.id.txtViewMaxHRValue);
 
 
         setSupportActionBar(findViewById(R.id.toolbar));
@@ -112,28 +129,6 @@ public class SleepReportActivity extends AppCompatActivity {
             nightEndTime = dbHandler.getNightEndTime(nightDate);
 
 
-            //LineGraphSeries<DataPoint> series = new LineGraphSeries<>();
-/*
-        int i = 0;
-        while (events.get(i).getType() == "Falling asleep") {
-            i++;
-        }
-
-        while (events.get(i).getType() == "normal") {
-            i++;
-        }
-
-        while (events.get(i).getType() == "apnea") {
-            i++;
-        }
-
-        while (events.get(i).getType() == "Awakening") {
-            i++;
-        }
-
- */
-
-
             // Create series for each event type
             LineGraphSeries<DataPoint> fallingAsleepSeries = new LineGraphSeries<>();
             LineGraphSeries<DataPoint> normalSeries = new LineGraphSeries<>();
@@ -141,42 +136,36 @@ public class SleepReportActivity extends AppCompatActivity {
             LineGraphSeries<DataPoint> awakeningSeries = new LineGraphSeries<>();
 
             // Iterate through events and add data points to respective series
-            for (int i = 0; i < events.size(); i++) {
-                Event event = events.get(i);
-                //double yValue = event.getBpm();
-                Date xValue = event.getDateAsDate(); // Use getDateAsDate() method
-                DataPoint dataPoint = new DataPoint(xValue.getTime(), event.getBpm());
+            for (Event event : events) {
+
+                DataPoint dataPoint = new DataPoint(event.getDateAsDate().getTime(), event.getBpm());
 
                 // Add data points to respective series based on event type
                 switch (event.getType()) {
                     case "Falling asleep":
                         fallingAsleepSeries.appendData(dataPoint, true, events.size());
-                        //color = getColorForEventType(event.getType());
-
                         normalSeries.appendData(dataPoint, true, events.size());
-
                         break;
+
                     case "normal":
                         normalSeries.appendData(dataPoint, true, events.size());
                         break;
+
                     case "apnea":
                         apneaSeries.appendData(dataPoint, true, events.size());
-
                         normalSeries.appendData(dataPoint, true, events.size());
-
                         break;
+
                     case "Awakening":
                         awakeningSeries.appendData(dataPoint, true, events.size());
-
                         normalSeries.appendData(dataPoint, true, events.size());
-
                         break;
                 }
             }
 
 
             fallingAsleepSeries.setColor(getResources().getColor(R.color.colorFallingAsleep));
-            fallingAsleepSeries.setThickness(18);
+            fallingAsleepSeries.setThickness(12);
             normalSeries.setColor(getResources().getColor(R.color.colorNormal));
             normalSeries.setThickness(12);
             apneaSeries.setColor(getResources().getColor(R.color.colorApnea));
@@ -184,9 +173,9 @@ public class SleepReportActivity extends AppCompatActivity {
             apneaSeries.setSize(30);
             apneaSeries.setShape(PointsGraphSeries.Shape.POINT); // Set the shape to POINT
 
-// Add each series to the graph
+            // Add each series to the graph
             awakeningSeries.setColor(getResources().getColor(R.color.colorAwakening));
-            awakeningSeries.setThickness(14);
+            awakeningSeries.setThickness(12);
 
             // Add each series to the graph
             graphView.addSeries(normalSeries);
@@ -194,8 +183,6 @@ public class SleepReportActivity extends AppCompatActivity {
             graphView.addSeries(apneaSeries);
             graphView.addSeries(awakeningSeries);
 
-            // on below line we are adding data to our graph view.
-            //LineGraphSeries<DataPoint> series = new LineGraphSeries<>(bpmDataPoints.toArray(new DataPoint[0]));
 
             // after adding data to our line graph series.
             // on below line we are setting
@@ -218,22 +205,23 @@ public class SleepReportActivity extends AppCompatActivity {
             // Customize graph properties if needed
             graphView.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(this)); // Format X-axis as date
 
-            // Set custom bounds for the vertical axis to ensure all data points are visible
-            double minY = findMinYValue(fallingAsleepSeries, normalSeries, awakeningSeries);
-            double maxY = findMaxYValue(fallingAsleepSeries, normalSeries, awakeningSeries);
-            graphView.getViewport().setYAxisBoundsManual(true);
-            graphView.getViewport().setMinY(minY-10);
-            graphView.getViewport().setMaxY(maxY+10);
 
-            // Adjust the number of horizontal labels to display the x-values between each 5 samples
-            int numLabels = Math.min(events.size(), 5); // Display labels for every 5 samples
-            //int numLabels = events.size();
+            graphView.getViewport().setYAxisBoundsManual(true);
+            graphView.getViewport().setMinX(normalSeries.getLowestValueX());
+            graphView.getViewport().setMaxX(normalSeries.getHighestValueX());
+            graphView.getViewport().setMinY(normalSeries.getLowestValueY());
+            graphView.getViewport().setMaxY(normalSeries.getHighestValueY());
+
+            // Adjust the number of horizontal labels to display the x-values between each n samples
+            int numLabels = Math.min(events.size(), 5); // Display labels for every n samples
+            //int numLabels = events.size()/4;
             graphView.getGridLabelRenderer().setNumHorizontalLabels(numLabels);
 
 
             // Show labels for each data point
             graphView.getGridLabelRenderer().setHumanRounding(false); // Disable rounding of labels
             graphView.getGridLabelRenderer().setHorizontalLabelsAngle(45); // Rotate labels for better visibility
+            graphView.getGridLabelRenderer().setTextSize(30);
 
             // Make the line thicker
             //series.setThickness(8); // Adjust the thickness as needed
@@ -244,27 +232,33 @@ public class SleepReportActivity extends AppCompatActivity {
 
             textViewApneaEvents.setText("No. suspected Apnea Events: " + numberApneaEvents);
 
-            textViewSleepStamps.setText("Sleep started at " + nightStartTime + " and ended at: " + nightEndTime
-            + "minY: " + minY + " maxY: " + maxY);
+            textViewSleepStamps.setText("Sleep started at " + nightStartTime + " and ended at: " + nightEndTime);
 
             if (numberApneaEvents == 0) {
                 textViewSleepQuality.setText("Looks like you had a night well rested!!");
                 imageViewSleepQuality.setImageResource(R.drawable.drowsy_doodle);
                 constraintLayout.setBackgroundResource(R.drawable.good_sleep_report_background);
+                toolbar.setBackgroundResource(R.drawable.good_sleep_report_background);
                 getSupportActionBar().setBackgroundDrawable(getResources().getDrawable(R.color.goodSleepBar));
                 textViewApneaEvents.setTextColor(getResources().getColor(R.color.goodSleepText));
                 textViewSleepQuality.setTextColor(getResources().getColor(R.color.goodSleepText));
-                //series.setColor(getResources().getColor(R.color.goodSleepText));
             } else {
                 textViewSleepQuality.setText("It seems you had a rough nigh...");
                 imageViewSleepQuality.setImageResource(R.drawable.confuse_doodle);
                 constraintLayout.setBackgroundResource(R.drawable.bad_sleep_report_background);
+                toolbar.setBackgroundResource(R.drawable.bad_sleep_report_background);
                 getSupportActionBar().setBackgroundDrawable(getResources().getDrawable(R.color.badSleepBar));
                 textViewApneaEvents.setTextColor(getResources().getColor(R.color.badSleepText));
                 textViewSleepQuality.setTextColor(getResources().getColor(R.color.badSleepText));
-                //series.setColor(getResources().getColor(R.color.badSleepText));
-
             }
+
+            textViewFallAsleepTime.setText(getFallAsleepTime(events) + " min");
+            textViewSleepTime.setText(getSleepTime(events) + " min");
+            textViewAwakeningTime.setText(getAwakeningTime(events) + " min");
+            textViewMinBPM.setText(getMinBPM(events) + " bpm");
+            textViewMedianBPM.setText((int) calculateEventBpmMedian(events) + " bpm");
+            textViewMaxBPM.setText(getMaxBPM(events) + " bpm");
+
         }
 
 
@@ -314,29 +308,80 @@ public class SleepReportActivity extends AppCompatActivity {
         return eventType.equals("Apnea") ? 10 : 5;
     }
 
-    // Method to find the minimum Y-value among all series
-    private double findMinYValue(LineGraphSeries<DataPoint>... series) {
-        double minY = Double.MAX_VALUE;
-        int i = 0;
-        for (LineGraphSeries<DataPoint> s : series) {
-            if (!s.isEmpty()) {
-                minY = Math.min(minY, s.getLowestValueY());
-                Toast.makeText(this, "Min Y:(" + i + ")" + minY, Toast.LENGTH_SHORT).show();
-                i++;
+    private int getFallAsleepTime (ArrayList<Event> events){
+        int time = 0;
+        for (Event event : events){
+            if (event.getType().equals("Falling asleep")){
+                time++;
             }
         }
-        return minY;
+        return time;
     }
 
-    // Method to find the maximum Y-value among all series
-    private double findMaxYValue(LineGraphSeries<DataPoint>... series) {
-        double maxY = Double.MIN_VALUE;
-        for (LineGraphSeries<DataPoint> s : series) {
-            if (!s.isEmpty()) {
-                maxY = Math.max(maxY, s.getHighestValueY());
+    private int getSleepTime (ArrayList<Event> events){
+        int time = 0;
+        for (Event event : events){
+            if (event.getType().equals("normal") || event.getType().equals("apnea")){
+                time++;
             }
         }
-        return maxY;
+        return time;
+    }
+
+    private int getAwakeningTime (ArrayList<Event> events){
+        int time = 0;
+        for (Event event : events){
+            if (event.getType().equals("Awakening")){
+                time++;
+            }
+        }
+        return time;
+    }
+
+    private int getMinBPM (ArrayList<Event> events){
+        int min = 1000;
+        for (Event event : events){
+            if (event.getBpm() < min){
+                min = (int) event.getBpm();
+            }
+        }
+        return min;
+    }
+
+    private int getMaxBPM (ArrayList<Event> events){
+        int max = 0;
+        for (Event event : events){
+            if (event.getBpm() > max){
+                max = (int) event.getBpm();
+            }
+        }
+        return max;
+    }
+
+    public static double calculateEventBpmMedian(ArrayList<Event> events) {
+        // Check for empty list
+        if (events == null || events.isEmpty()) {
+            throw new IllegalArgumentException("The list is empty");
+        }
+
+        // Sort the ArrayList
+        ArrayList<Event> sortedEvents = new ArrayList<>(events);
+        sortedEvents.sort((e1, e2) -> Double.compare(e1.getBpm(), e2.getBpm()));
+
+        int size = sortedEvents.size();
+        double median;
+
+        if (size % 2 == 0) {
+            // If the size is even, average the two middle elements
+            double middle1 = sortedEvents.get(size / 2 - 1).getBpm();
+            double middle2 = sortedEvents.get(size / 2).getBpm();
+            median = (middle1 + middle2) / 2.0;
+        } else {
+            // If the size is odd, take the middle element
+            median = sortedEvents.get(size / 2).getBpm();
+        }
+
+        return median;
     }
 
 
